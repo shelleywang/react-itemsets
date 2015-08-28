a8fbfe8c5cfebb459096776637c4955ea5ed8c88
Shelley Wang <shelleyw13@gmail.com>
2015-08-27 21:15:51 -0700

    Add beginning files

---
+++ server/config/sampleconfig.js
@@ -0,0 +1,7 @@
+// Obtain a key for the League of Legends API from Riot
+// https://developer.riotgames.com/
+var LeagueOfLegendsKey = "YOUR_KEY_HERE";
+
+module.exports = {
+  leagueKey : LeagueOfLegendsKey
+}
